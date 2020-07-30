3c19b3145875da8acb713a92bbf6c0ecf16bf697
aiza31 <aziret.sherov@gmail.com>
2020-07-30 16:28:56 +0600

    Counter done

---
+++ src/components/BottomBun.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BottomBun = () => {
+    return (
+        <div className="BreadBottom"/>
+    );
+};
+
+export default BottomBun;
---
+++ src/components/Ingredient.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cheese = (props) => {
+    return (
+        <div className=/*"Cheese"*/{props.Class}/>
+    );
+};
+
+export default Cheese;
